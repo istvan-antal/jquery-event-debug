8da319ae85d9b77c6b216d1484894d448210a9da
István Miklós Antal <istvan.m.antal@gmail.com>
2011-06-14 20:50:14 +0200

    Captured jQuery.event.handle method.

--- jquery-event-debug.js
+++ jquery-event-debug.js
@@ -1,8 +1,14 @@
 (function () {
-    var _trigger = jQuery.event.trigger;
+    var _trigger = jQuery.event.trigger,
+        _handle = jQuery.event.handle;
     
     jQuery.event.trigger = function( event, data, elem, onlyHandlers ) {
         console.log('Event:',  event);
         _trigger.apply(jQuery.event, arguments);
+    };
+    
+    jQuery.event.handle = function( event ) {
+        console.log('Event:',  event);
+        _handle.apply(jQuery.event, arguments);
     };
 }());
